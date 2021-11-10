9616179d3645b7aa85fa1b2066f64da4ec64fa06
UwUssimo Robinson <uwussimo@icloud.com>
2021-11-10 17:32:57 +0500

    tray icon dark mode detection added

--- main.js
+++ main.js
@@ -1,54 +1,95 @@
-const { app, BrowserWindow, Menu, Tray, clipboard } = require('electron')
-const path = require('path')
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  Tray,
+  nativeImage,
+  nativeTheme,
+} = require("electron");
+const path = require("path");
 
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
 let mainWindow = null;
+const darkMode = require("electron").nativeTheme.shouldUseDarkColors;
+const lightIcon = nativeImage.createFromPath(
+  path.join(__dirname + "/icons/tray", "tray-22.png")
+);
+const darkIcon = nativeImage.createFromPath(
+  path.join(__dirname + "/icons/tray", "tray-dark-22.png")
+);
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 200,
     height: 200,
     transparent: true,
     fullscreenable: false,
-    backgroundColor: '#01FFFFFF',
+    backgroundColor: "#01FFFFFF",
     hasShadow: false,
     frame: false,
     resizable: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  mainWindow.loadFile('index.html').then(r => console.log(r))
+  mainWindow.loadFile("index.html").then((r) => console.log(r));
   mainWindow.setAlwaysOnTop(true);
 }
 
-let tray = null
-function createTray () {
-  tray = new Tray(path.join(__dirname + '/icons/tray', 'tray-osx.png'))
+let tray = null;
+function createTray() {
+  if (darkMode) {
+    tray = new Tray(darkIcon);
+  } else {
+    tray = new Tray(lightIcon);
+  }
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Make slave work', click: function () { mainWindow.show() } },
-    { label: 'Hide slave for a while', click: function () { mainWindow.hide() } },
-    { label: 'Give a rest to slave', click: function () { app.quit() } },
-  ])
-  tray.setToolTip('Slave Tooltips.')
-  tray.setContextMenu(contextMenu)
+    {
+      label: "Make slave work",
+      click: function () {
+        mainWindow.show();
+      },
+    },
+    {
+      label: "Hide slave for a while",
+      click: function () {
+        mainWindow.hide();
+      },
+    },
+    {
+      label: "Give a rest to slave",
+      click: function () {
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("Slave Tooltips.");
+  tray.setContextMenu(contextMenu);
+}
+
+function updateMyAppTheme(isDark) {
+  tray.setImage(isDark ? darkIcon : lightIcon);
 }
 
 app.whenReady().then(() => {
-  createTray()
-  createWindow()
+  createTray();
+  createWindow();
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+nativeTheme.on("updated", function theThemeHasChanged() {
+  updateMyAppTheme(nativeTheme.shouldUseDarkColors);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
 app.dock.hide();
--- readme.md
+++ readme.md
@@ -13,29 +13,32 @@ working process.**
 
 ## Documentations
 
--   **Electron.js** - [Website](https://www.electronjs.org/) | [GiHub](https://github.com/electron)
+- **Electron.js** - [Website](https://www.electronjs.org/) | [GiHub](https://github.com/electron)
 
 ## Contributors
 
 This bot exists thankfully for those who contributed this project and they are:
 
--   [UwUssimo](https://github.com/uwussimo) _(Full Master)_ - The actual creator
-    of the project and active contributor
+- [UwUssimo](https://github.com/uwussimo) _(Full Master)_ - The actual creator
+  of the project and active contributor
 
 > Start contributing to become relative to maid!
+
 ---
 
 > The app page is still in beta phase so feel free to
 > [open issues](https://github.com/mad-maids/maid.slave/issues/new) and give me some
 > suggestions.
+
 ---
 
-> **Important Note:** Feel free to use this app's resources in your own projects, but 
+> **Important Note:** Feel free to use this app's resources in your own projects, but
 > please **don't** use it in any commercial project. Give us some credit and point
 > [(this repository)](https://github.com/mad-maids/maid.slave). If you're modifying
 > this source and making your own app, you have to ask for permission from
 > creator "UwUssimo" according to the CC0-1.0 license. Check [LICENSE](license)
 > for more info.
+
 <p align="center">Copyright &copy; 2020-2021 <a href="https://maid.uz" target="_blank">Mad Maids</a></p>
 
 <p align="center"><a href="https://github.com/mad-maids/maid.ts/blob/master/license"><img src="https://img.shields.io/static/v1.svg?style=flat-square&label=License&message=CC0-1.0&logoColor=eceff4&logo=github&colorA=000000&colorB=ffffff"/></a></p>
